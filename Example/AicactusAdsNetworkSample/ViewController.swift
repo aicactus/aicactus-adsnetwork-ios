fce3c88bdf23d5f11c2b738d5dbbfd531da6ec27
Si Huynh <t.si@resola.ai>
2022-04-04 12:52:42 +0700

    Using AicactusAdsNetwork v1.2.0

--- Example/AicactusAdsNetworkSample/ViewController.swift
+++ Example/AicactusAdsNetworkSample/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        AicactusAdsNetwork.shared.setup("") { result in
+        AicactusAdsNetwork.shared.setup("b8a3ccf2-5d49-4912-b2cc-87dc46e10277") { result in
             switch result {
             case .success(let container):
                 print("Container ID \(container.id)")
@@ -21,10 +21,21 @@ class ViewController: UIViewController {
                 print("Failed to init: \(error)")
             }
         }
-        
-        
     }
 
+}
 
+extension ViewController: AdViewDelegate {
+    func adView(_ adView: AdView, didFailLoad error: AdsNetworkSDKError) {
+        
+    }
+    
+    func adView(_ adView: AdView, didSuccessLoad bid: Bid) {
+        
+    }
+    
+    func adView(_ adView: AdView, didClickAd url: URL) {
+        
+    }
 }
 
